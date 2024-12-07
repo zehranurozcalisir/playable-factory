f525fe2958e888236ff26d01ca3ac5e609512f2a
ZEHRA NUR <99538165+zehranurozcalisir@users.noreply.github.com>
2024-12-07 15:45:53 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,24 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Login from "./pages/Login.tsx";
 import ProtectedRoute from "./components/ProtectedRoute.tsx";
 import checkAuth from "./utils/checkAuth.ts";
 import NotFound from "./pages/NotFound.tsx";
 import Navbar from "./components/Navbar.tsx";
-import Cookies from 'js-cookie';
 
 function App() {
     const isToken = checkAuth();
     const {pathname, search, hash} = window.location;
-    const showNavbar = pathname === "/home" || pathname === "/"  ? true : false;
-    const isAuthenticated = Cookies.get('token');
+    const showNavbar = pathname === "/home" ? true : false;
     return (
         <>
-            {showNavbar && <Navbar/>}
             <Router>
                 <Routes>
-                    <Route
-                        path="/"
-                        element={<Navigate to={isAuthenticated ? "/home" : "/login"} replace />}
-                    />
+                <Route path="/" element={<ProtectedRoute isToken={isToken} option={0}>
+                        <Home/>
+                    </ProtectedRoute>}/>
                     <Route path="/home" element={<ProtectedRoute isToken={isToken} option={0}>
                         <Home/>
                     </ProtectedRoute>}/>
