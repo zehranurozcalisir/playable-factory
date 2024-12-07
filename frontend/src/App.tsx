4c2cc9f8a6a0d7de403e583f3a83f9785ffa2e33
ZEHRA NUR <99538165+zehranurozcalisir@users.noreply.github.com>
2024-12-07 15:27:46 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Login from "./pages/Login.tsx";
 import ProtectedRoute from "./components/ProtectedRoute.tsx";
 import checkAuth from "./utils/checkAuth.ts";
 import NotFound from "./pages/NotFound.tsx";
 import Navbar from "./components/Navbar.tsx";
+import Cookies from 'js-cookie';
 
 function App() {
     const isToken = checkAuth();
     const {pathname, search, hash} = window.location;
     const showNavbar = pathname === "/home" ? true : false;
+    const isAuthenticated = Cookies.get('token');
     return (
         <>
             {showNavbar && <Navbar/>}
             <Router>
                 <Routes>
+                    <Route
+                        path="/"
+                        element={<Navigate to={isAuthenticated ? "/home" : "/login"} replace />}
+                    />
                     <Route path="/home" element={<ProtectedRoute isToken={isToken} option={0}>
                         <Home/>
                     </ProtectedRoute>}/>
